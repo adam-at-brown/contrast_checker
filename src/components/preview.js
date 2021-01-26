c6d76921470c43de907912d32430d5a2033f51ff
Adam Holtzman <adam_holtzman@brown.edu>
2021-01-26 11:24:41 -0500

    add font sizes for visual refernece

--- src/components/preview.js
+++ src/components/preview.js
@@ -1,5 +1,14 @@
 import styled from "styled-components";
 
+const FontSizes = () => (
+  <>
+    <p style={{ fontSize: 12}}>small font size 12 <b>& bold</b></p>
+    <p style={{ fontSize: 14}}>medium font size 14 <b>& bold</b></p>
+    <p style={{ fontSize: 18}}>large font size 18 <b>& bold</b></p>
+    <p style={{ fontSize: 24}}>extra large font size 24 <b>& bold</b></p>
+  </>
+)
+
 export default function Preview({ color1, color2 }) {
   return (
     <Wrap>
@@ -7,11 +16,13 @@ export default function Preview({ color1, color2 }) {
         <p>
           Text: {color2} Background: {color1}
         </p>
+        <FontSizes />
       </Block>
       <Block bg={color2} color={color1}>
         <p>
           Text: {color1} Background: {color2}
         </p>
+        <FontSizes />
       </Block>
     </Wrap>
   );
@@ -31,4 +42,8 @@ const Block = styled.div`
   padding: 1rem;
   flex: 1;
   margin: 1px;
+
+  p {
+    margin: 10px 0;
+  }
 `;
